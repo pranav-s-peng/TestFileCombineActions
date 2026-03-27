d1410fe2f50746a1c18b9d99fce56ec2b0fa108d
pranav-s-peng <130057846+pranav-s-peng@users.noreply.github.com>
2026-03-27 05:14:33 +0000

    Auto-generated SQL bundle (Release) [SP: 2, Data: 0]

---
+++ Output/Main/20260327051432__TestFileCombineActions.sql
@@ -0,0 +1,24 @@
+-- DB/SP/AppleSP.sql
+GO
+CREATE OR ALTER PROCEDURE AppleSP
+(
+	@FoodCount INT
+)
+AS
+BEGIN
+	SELECT 'Testing testing 1 2 3 crunch'
+END
+
+-- DB/SP/OrangeSP.sql
+GO
+CREATE OR ALTER PROCEDURE OrangeSP
+(
+	@FoodCount INT
+)
+AS
+BEGIN
+	SELECT 'Testing testing 1 2 3 swish'
+END
+
+-- End of changes
+GO
